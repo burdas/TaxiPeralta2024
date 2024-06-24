5e0aa21a1f76b565a285c5ac5dc98d680392dc15
burdas <burdasparmarcos@gmail.com>
2024-06-24 16:09:55 +0200

    Create NavBar and Dark theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
