b6c9bc508928ba2a5de2cece3760c093b772c99b
burdas <burdasparmarcos@gmail.com>
2024-06-25 00:21:20 +0200

    Create a Dialog menu for mobile devices

---
+++ src/model/navbar.tsx
@@ -0,0 +1,35 @@
+export interface RouteProps {
+  href: string;
+  label: string;
+}
+
+export const routeList: RouteProps[] = [
+  {
+    href: "#Informacion",
+    label: "Información",
+  },
+  {
+    href: "#Galeria",
+    label: "Galeria",
+  },
+  {
+    href: "#Tarifas",
+    label: "Tarifas",
+  },
+  {
+    href: "#Caluladora",
+    label: "Calcula tu viaje",
+  },
+  {
+    href: "#Enlaces",
+    label: "Enlaces",
+  },
+  {
+    href: "#FiestasPeralta",
+    label: "Fiestas de Peralta",
+  },
+  {
+    href: "#Contacto",
+    label: "Contacto",
+  },
+];
