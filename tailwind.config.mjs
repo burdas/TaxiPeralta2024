d4989a27b136c6b87e8d5b84b8d02eae450fcf85
burdas <burdasparmarcos@gmail.com>
2024-07-02 00:18:02 +0200

    Complete galery section with modal image

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     container: {
       center: true,
@@ -11,14 +11,22 @@ export default {
       },
     },
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
     extend: {
-      ringColor: ['hover', 'active', 'focus'],
+      animation: {
+        modalf: "modalf 0.30s ease-in-out",
+      },
+      keyframes: {
+        modalf: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
     },
   },
   plugins: [],
