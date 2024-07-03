606fc9d826fea21cf8a599f0f285fdf283fc1ff7
burdas <burdasparmarcos@gmail.com>
2024-07-03 16:29:57 +0200

    Add markers to map

--- src/utils/map.ts
+++ src/utils/map.ts
@@ -1,6 +1,9 @@
 import { Loader } from "@googlemaps/js-api-loader";
 import { showDangerToast } from "./Toast";
 
+let map: google.maps.Map;
+let loader: Loader;
+let bounds: google.maps.LatLngBounds;
 const HOME = { lat: 42.339044, lng: -1.806348 };
 const PERALTA = { lat: 42.3389757, lng: -1.7990956999999526 };
 const ROUTES_COLORS = [
@@ -15,19 +18,22 @@ const ROUTES_COLORS = [
 export async function createMap(
   elementId: string,
   center = PERALTA,
-  zoom = 15
+  zoom = 15,
+  mapId = 'MAP_ID'
 ): Promise<google.maps.Map> {
-  const loader = new Loader({
+  loader = new Loader({
     apiKey: import.meta.env.PUBLIC_GOOGLE_MAPS_API as string,
     version: "weekly",
     libraries: ["places"],
   });
 
   const { Map } = await loader.importLibrary("maps");
-  return new Map(document.getElementById(elementId) as HTMLElement, {
+  map = new Map(document.getElementById(elementId) as HTMLElement, {
     center,
     zoom,
+    mapId
   });
+  return map
 }
 
 export const getAutoComplete = (el: string) =>
@@ -35,6 +41,56 @@ export const getAutoComplete = (el: string) =>
     document.getElementById(el) as HTMLInputElement
   );
 
-export function showMarkPoint(map: google.maps.Map) {
-  showDangerToast("No se ha podido encontrar la ubicación.");
+export async function showOriginPoint(
+  this: google.maps.places.Autocomplete
+) {
+  const place = this.getPlace();
+  if (!place.geometry) {
+    showDangerToast(
+      `No se ha podido encontrar "${place.name}". Selecciona un lugar de la lista.`
+    );
+    return;
+  }
+  bounds = new google.maps.LatLngBounds();
+  if (place.geometry.viewport) {
+    map.fitBounds(place.geometry.viewport);
+  } else {
+    map.setCenter(place.geometry.location!);
+    map.setZoom(15);
+  }
+
+  const { AdvancedMarkerElement } = await loader.importLibrary("marker");
+  const marker = new AdvancedMarkerElement({
+    map,
+    position: place.geometry.location,
+    title: "Origen",
+  });
+  bounds.extend(marker.position!);
+}
+
+export async function showDestinyPoint(
+  this: google.maps.places.Autocomplete
+) {
+  const place = this.getPlace();
+  if (!place.geometry) {
+    showDangerToast(
+      `No se ha podido encontrar "${place.name}". Selecciona un lugar de la lista.`
+    );
+    return;
+  }
+  if (place.geometry.viewport) {
+    map.fitBounds(place.geometry.viewport);
+  } else {
+    map.setCenter(place.geometry.location!);
+    map.setZoom(15);
+  }
+
+  const { AdvancedMarkerElement } = await loader.importLibrary("marker");
+  const marker = new AdvancedMarkerElement({
+    map,
+    position: place.geometry.location,
+    title: "Origen",
+  });
+  bounds.extend(marker.position!);
+  map.fitBounds(bounds);
 }
