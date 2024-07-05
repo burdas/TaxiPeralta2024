88ceae9b9c075b19dbcf5c69e4ff6845b5c76e31
burdas <burdasparmarcos@gmail.com>
2024-07-05 14:54:18 +0200

    Implement old map style. To do, change to new google cloud style

--- src/utils/map.ts
+++ src/utils/map.ts
@@ -1,5 +1,6 @@
 import { Loader } from "@googlemaps/js-api-loader";
 import { showDangerToast } from "./Toast";
+import { HOME, PERALTA, ROUTES_COLORS, DARK_MAP} from "./MapConst";
 
 let map: google.maps.Map;
 let loader: Loader;
@@ -12,23 +13,13 @@ const origenDestino: OrigenDestino = {
   origen: null,
   destino: null,
 };
-const HOME = { lat: 42.339044, lng: -1.806348 };
-const PERALTA = { lat: 42.3389757, lng: -1.7990956999999526 };
-const ROUTES_COLORS = [
-  "#fe0c0c",
-  "#2534d7",
-  "#17a416",
-  "#f5db0a",
-  "#750f68",
-  "#00FFFF",
-];
 
 // Create a new Loader instance y create a new Map instance y devuelve la instancia map
 export async function createMap(
   elementId: string,
   center = PERALTA,
   zoom = 15,
-  mapId = "MAP_ID"
+  //mapId = "MAP_ID"
 ): Promise<google.maps.Map> {
   loader = new Loader({
     apiKey: import.meta.env.PUBLIC_GOOGLE_MAPS_API as string,
@@ -40,7 +31,8 @@ export async function createMap(
   map = new Map(document.getElementById(elementId) as HTMLElement, {
     center,
     zoom,
-    mapId,
+    //mapId,
+    styles: document.documentElement.classList.contains('dark') ? DARK_MAP : null
   });
   return map;
 }
