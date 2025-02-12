c42b6a204b7868344a8890d8dca020d1d3893e05
burdas <burdasparmarcos@gmail.com>
2025-02-12 15:00:51 +0100

    Fix travel duration

--- src/utils/Format.ts
+++ src/utils/Format.ts
@@ -7,9 +7,8 @@ export function capitalizeFirstLetter(s: string) {
 }
 
 export const secToTimeFormat = (seconds: number): string => {
-    const date = new Date(seconds * 1000);
-    const h = date.getDate();
-    const m = date.getMinutes();
-    const s = date.getSeconds();
+    const h = Math.floor(seconds / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = seconds % 60;
     return (h === 0 ? '' : `${h}h `) + (m === 0 ? '' : `${m}m `) + (s === 0 ? '' : `${s}s`)
 };
