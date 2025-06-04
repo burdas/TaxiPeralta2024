efcb147d1a37a6c5f30b7fc1655bbc2212072c14
burdas <burdasparmarcos@gmail.com>
2025-06-04 19:47:22 +0200

    Añadido ruta callback segun entorno

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,7 +1,14 @@
 import { Google } from "arctic";
 
+const baseUrl =
+	process.env.NODE_ENV === "production"
+		? import.meta.env.IS_PREVIEW ? "https://taxi-peralta2024-3pc3.vercel.app" : "https://taxiperalta.com"
+		: "http://localhost:4321";
+
+console.log(baseUrl);
+
 export const google = new Google(
 	import.meta.env.GOOGLE_CLIENT_ID,
 	import.meta.env.GOOGLE_CLIENT_SECRET,
-	"https://taxi-peralta2024-3pc3.vercel.app/login/callback"
+	`${baseUrl}/login/callback`
 );
