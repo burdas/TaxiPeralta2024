e2d6b047f8ea3a8af144059b2766d2fd961aeefb
burdas <burdasparmarcos@gmail.com>
2025-06-04 19:29:50 +0200

    Prueba

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -3,5 +3,5 @@ import { Google } from "arctic";
 export const google = new Google(
 	import.meta.env.GOOGLE_CLIENT_ID,
 	import.meta.env.GOOGLE_CLIENT_SECRET,
-	"http://localhost:4321/login/callback"
+	"https://taxi-peralta2024-3pc3.vercel.app/login/callback"
 );
